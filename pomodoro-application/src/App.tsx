c499f5e0462bbbe7065c4fd9d5e7819fd1d710d9
Caio Theodoro de Carvalho <caio.theodoroc@gmail.com>
2023-03-12 12:18:49 -0300

    first setup

---
+++ pomodoro-application/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1>
+      Projeto criado
+    </h1>
+   </div>
+  )
+}
