f0f6a64e397994b772e3f95b1b2ce138fa0841ac
Caio Theodoro de Carvalho <caio.theodoroc@gmail.com>
2023-03-12 13:11:13 -0300

    defining defaultTheme

--- pomodoro-application/src/components/Button.styles.ts
+++ pomodoro-application/src/components/Button.styles.ts
@@ -17,7 +17,11 @@ const ButtonVariants = {
 export const ButtonContainer = styled.button<ButtonConainerProps>`
     width: 100px;
     height: 40px;
-    background-color: ${({theme}) => theme.secondary};
+    border-radius: 4px;
+    border: 0;
+    margin:8px;
+    background-color: ${({theme}) => theme['green-500']};
+    color: ${({theme})=>theme.white};
 
     /* ${({variant}) => css`background-color:${ButtonVariants[variant]}`} */
 `;
