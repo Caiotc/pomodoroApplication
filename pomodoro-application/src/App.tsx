82a062eaf85a79abc6a422c4b903b8120413b83b
Caio Theodoro de Carvalho <caio.theodoroc@gmail.com>
2023-03-13 21:42:34 -0300

    config routes

--- pomodoro-application/src/App.tsx
+++ pomodoro-application/src/App.tsx
@@ -1,17 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
